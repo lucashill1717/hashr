4a8f5cb9498725d498ffc0363c7a38779076c5b8
lucashill1717 <lucashill1717@gmail.com>
2023-12-21 00:06:29 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hashr",
+    version="0.0.1",
+    author="Lucas Hill",
+    description="Custom hash function for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["hashr"],
+    package_dir={'':'hashr/src'},
+    install_requires=[]
+)
