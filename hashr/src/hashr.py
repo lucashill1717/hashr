90c659ab47c21891c0d92a3083308ce6c9d93937
lucashill1717 <lucashill1717@gmail.com>
2023-12-23 03:16:49 -0700

    saving weird progress

--- hashr/src/hashr.py
+++ hashr/src/hashr.py
@@ -1,7 +1,17 @@
 import sys
 
-def hash(string):
+def hashr(string):
+    total = 0
+    prime = 509
+    for char in string:
+        total += ord(char)
+        total *= prime
+    total_bytes = total.to_bytes((total.bit_length()+7)//8, "big")
+    hexes = [hex(byte) for byte in total_bytes]
+    characters = [chr(int(hex, 16)) for hex in hexes]
+    print(characters)
     return string
 
 if __name__ == "__main__":
-    print(hash(sys.argv[1]))
+    # reminder to add salt to hash
+    print(hashr(sys.argv[1]))
