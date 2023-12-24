86979d63a105420deda90ee65c003d773ee99691
lucashill1717 <lucashill1717@gmail.com>
2023-12-24 01:59:30 -0700

    black changes

--- hashr/src/hashr.py
+++ hashr/src/hashr.py
@@ -2,29 +2,30 @@ import sys
 
 
 letters = {
-    '1': 'a',
-    '2': 'b',
-    '3': 'c',
-    '4': 'd',
-    '5': 'e',
-    '6': 'f',
-    '7': 'g',
-    '8': 'h',
-    '9': 'i',
-    '0': 'j'
+    "1": "a",
+    "2": "b",
+    "3": "c",
+    "4": "d",
+    "5": "e",
+    "6": "f",
+    "7": "g",
+    "8": "h",
+    "9": "i",
+    "0": "j",
 }
+
 
 def hashr(string):
     total = 0
     primes = [5, 11, 31, 127, 709, 5381, 52711, 648391]
     point = 0
-    
+
     for char in string:
         total += ord(char)
         total *= primes[point]
         point += 1 if point < 7 else -7
 
-    total_string = ''.join([letters[num] for num in str(total)])
+    total_string = "".join([letters[num] for num in str(total)])
     return total_string
 
 
