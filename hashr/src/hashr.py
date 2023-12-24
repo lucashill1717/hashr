c19fa1a4d6510b856daacfda983e86b2b2c83aa9
lucashill1717 <lucashill1717@gmail.com>
2023-12-24 15:56:55 -0700

    more complexity

--- hashr/src/hashr.py
+++ hashr/src/hashr.py
@@ -2,30 +2,34 @@ import sys
 
 
 letters = {
-    "1": "a",
-    "2": "b",
-    "3": "c",
-    "4": "d",
-    "5": "e",
-    "6": "f",
-    "7": "g",
-    "8": "h",
-    "9": "i",
-    "0": "j",
+    "1": ["a", "k"],
+    "2": ["b", "l"],
+    "3": ["c", "m"],
+    "4": ["d", "n"],
+    "5": ["e", "o"],
+    "6": ["f", "p"],
+    "7": ["g", "q"],
+    "8": ["h", "r"],
+    "9": ["i", "s"],
+    "0": ["j", "t"],
 }
 
 
-def hashr(string):
+def hashr(string: str) -> str:
     total = 0
     primes = [5, 11, 31, 127, 709, 5381, 52711, 648391]
-    point = 0
+    point = 7
 
     for char in string:
         total += ord(char)
         total *= primes[point]
-        point += 1 if point < 7 else -7
-
-    total_string = "".join([letters[num] for num in str(total)])
+        point -= 1 if point > 0 else -7
+        
+    total_string = ""
+    flip = 0
+    for num in str(total):
+        total_string += letters[num][flip]
+        flip = 1 - flip
     return total_string
 
 
