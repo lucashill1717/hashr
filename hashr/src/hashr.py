aa6cfa41f44a7f54bfa52bb79f7b98279d9972df
lucashill1717 <lucashill1717@gmail.com>
2023-12-24 01:58:57 -0700

    rudimentary hash working

--- hashr/src/hashr.py
+++ hashr/src/hashr.py
@@ -1,17 +1,32 @@
 import sys
 
+
+letters = {
+    '1': 'a',
+    '2': 'b',
+    '3': 'c',
+    '4': 'd',
+    '5': 'e',
+    '6': 'f',
+    '7': 'g',
+    '8': 'h',
+    '9': 'i',
+    '0': 'j'
+}
+
 def hashr(string):
     total = 0
-    prime = 509
+    primes = [5, 11, 31, 127, 709, 5381, 52711, 648391]
+    point = 0
+    
     for char in string:
         total += ord(char)
-        total *= prime
-    total_bytes = total.to_bytes((total.bit_length()+7)//8, "big")
-    hexes = [hex(byte) for byte in total_bytes]
-    characters = [chr(int(hex, 16)) for hex in hexes]
-    print(characters)
-    return string
+        total *= primes[point]
+        point += 1 if point < 7 else -7
+
+    total_string = ''.join([letters[num] for num in str(total)])
+    return total_string
+
 
 if __name__ == "__main__":
-    # reminder to add salt to hash
     print(hashr(sys.argv[1]))
