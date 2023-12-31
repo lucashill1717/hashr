173d622c4ab39c912c45b780f69f8b7b5de8c602
lucashill1717 <lucashill1717@gmail.com>
2023-12-31 01:18:45 -0700

    changing minor things

--- hashr/hashr.py
+++ hashr/hashr.py
@@ -1,53 +1,59 @@
-letters = {
-    "1": ["a", "k"],
-    "2": ["b", "l"],
-    "3": ["c", "m"],
-    "4": ["d", "n"],
-    "5": ["e", "o"],
-    "6": ["f", "p"],
-    "7": ["g", "q"],
-    "8": ["h", "r"],
-    "9": ["i", "s"],
-    "0": ["j", "t"],
-}
+def number_to_letters(number: int) -> str:
+    letters = {
+        "1": ["a", "k"],
+        "2": ["b", "l"],
+        "3": ["c", "m"],
+        "4": ["d", "n"],
+        "5": ["e", "o"],
+        "6": ["f", "p"],
+        "7": ["g", "q"],
+        "8": ["h", "r"],
+        "9": ["i", "s"],
+        "0": ["j", "t"],
+    }
+    
+    chars = []
+    flip = 0
+    for num in str(number):
+        chars.append(letters[num][flip])
+        flip = 1 - flip
+    return "".join(chars)
 
 
-def saltr() -> str:
+def saltr(letters=False) -> int:
     """
     salt
     """
-    return ""
+
+    from random import randint
+
+    return randint(1000000, 9999999)
 
 
-def hashr(input: str, salt=False) -> str | list[str]:
+def hashr(input: str, use_salt=False) -> str | list[str]:
     """
     remember to document
     """
-    
-    if len(input) == 0: return ""
-    
+
+    if len(input) == 0:
+        return ""
+
     def hash(string: str, current_sum: int) -> int:
         primes = [5, 11, 31, 127, 709, 5381, 52711, 648391]
         point = 7
-        
+
         for char in string:
             current_sum += ord(char)
             current_sum *= primes[point]
             point -= 1 if point > 0 else -7
         return current_sum
-    
+
     total = 0
     total += hash(input, total)
     total += hash(str(total), total)
-    
     total %= 10888869450418352160768000001
 
-    total_string = ""
-    flip = 0
-    for num in str(total):
-        total_string += letters[num][flip]
-        flip = 1 - flip
-    return total_string
+    return number_to_letters(total)
 
 
 def _main():
