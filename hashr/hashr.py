0e6c2ed413c47963123e89640a711f78a430b5db
lucashill1717 <lucashill1717@gmail.com>
2024-01-01 02:15:19 -0700

    --help

--- hashr/hashr.py
+++ hashr/hashr.py
@@ -115,9 +115,23 @@ def hashr(input: str, use_salt=False) -> str | tuple[str, str]:
 def _main() -> None:
     from argparse import ArgumentParser
 
-    parser = ArgumentParser(description="testing")
-    parser.add_argument("input", help="string")
-    parser.add_argument("-s", "--use-salt", action="store_true", help="salt")
+    parser = ArgumentParser(
+        description="Custom hashing algorithm using two-pass character folding\
+                    and large prime multiplication and reduction.",
+        epilog="As I worked on this project alone and do not have a doctorate in\
+                cybersecurity, I am unsure if this program is suitable for enterprise\
+                systems. Through my testing it has proved its worth, but use caution.",
+    )
+    parser.add_argument(
+        "input",
+        help="string to be hashed, surround with quotes if it contains spaces.",
+    )
+    parser.add_argument(
+        "-s",
+        "--use-salt",
+        action="store_true",
+        help="use to add salt to the hash, causing even identical strings to haveseparate hashes. save the salt for later identification.",
+    )
     args = parser.parse_args()
 
     hash = hashr(args.input, args.use_salt)
